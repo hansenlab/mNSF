efbd10c6d92cfa66bfc782066c94351b53cc2081
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2023-03-20 19:48:00 -0400

    Add files via upload

---
+++ functions/load_packages.py
@@ -0,0 +1,139 @@
+
+
+##################################################################
+##################################################################
+
+#import psutil
+import numpy as np
+import os
+import sys 
+import pickle
+import pandas as pd
+import statistics
+import shutil
+import numpy as np
+import random
+
+from os import path
+from scanpy import read_h5ad
+import pandas as pd
+
+
+import tensorflow_probability as tfp
+import tensorflow as tf
+from tensorflow_probability import math as tm
+from tensorflow.data import Dataset
+tfk = tm.psd_kernels
+tv = tfp.util.TransformedVariable
+tv = tfp.util.TransformedVariable
+tfb = tfp.bijectors
+tfk = tm.psd_kernels
+ker = tfk.MaternThreeHalves
+
+
+
+hmkw = {"figsize":(7,.9),"bgcol":"white","subplot_space":0.1,"marker":"s","s":10}
+
+
+
+############################3
+dir_NSF_functions='/dcs04/legacy-dcs01-hansen/hansen_lab1/ywang/ST/May6_2022_sNMF_2samples/nsf-paper-main_2samples_LukasData_sample2_3_qsub_fasterVersion_sub500_v2_topleft_500genes_layer1to4')
+dir_mNSF_functions='/dcs04/legacy-dcs01-hansen/hansen_lab1/ywang/ST/May6_2022_sNMF_2samples/nsf-paper-main_2samples_rotate_regularSizeData_SSlayer_modZ_nSamples_memorySaving_byBatches')
+#os.chdir('/dcs04/legacy-dcs01-hansen/hansen_lab1/ywang/ST/May6_2022_sNMF_2samples/nsf-paper-main_2samples_rotate_regularSizeData_SSlayer_modZ_nSamples_memorySaving_byBatches')
+
+
+
+############################ load mNSF functions
+sys.path.append(dir_mNSF_functions)
+
+from models import pf_ori_mod
+from utils.misc import t2np
+from utils import preprocess,misc,training,visualize,postprocess
+#from models import cf,pf,pfh
+#from utils import preprocess,misc,training,visualize,postprocess
+from models import pf_fit12
+from models import pf_fit12_fullSample_2_3
+from utils import training_oneSample
+from utils import training_ori
+
+
+
+############################ load NSF functions
+sys.path.append(dir_NSF_functions)
+
+from models import cf,pf,pfh
+
+from utils import preprocess,misc,training,visualize,postprocess
+
+
+#######################################################################################
+
+
+def save_object(obj, filename):
+    with open(filename, 'wb') as outp:  # Overwrites any existing file.
+        pickle.dump(obj, outp, pickle.HIGHEST_PROTOCOL)
+        
+        
+def interpret_npf_v3(list_fit,list_X,S=10,**kwargs):
+  """
+  fit: object of type PF with non-negative factors
+  X: spatial coordinates to predict on
+  returns: interpretable loadings W, factors eF, and total counts vector
+  """
+  kk=0
+  for fit_tmp in list_fit:
+    kk=kk+1
+  for kkk in range(0,kk):
+    Fhat_tmp = misc.t2np(list_fit[kkk].sample_latent_GP_funcs(list_X[kkk],S=S,chol=False)).T #NxL
+    if kkk==0:
+      Fhat_c=Fhat_tmp
+    else:
+      Fhat_c=np.concatenate((Fhat_c,Fhat_tmp), axis=0)
+  return interpret_nonneg(np.exp(Fhat_c),list_fit[kkk].W.numpy(),sort=False,**kwargs)
+
+
+
+def interpret_nonneg(factors,loadings,lda_mode=False,sort=True):
+  """
+  Rescale factors and loadings from a nonnegative factorization
+  to improve interpretability. Two possible rescalings:
+
+  1. Soft clustering of observations (lda_mode=True):
+  Rows of factor matrix sum to one, cols of loadings matrix sum to one
+  Returns a dict with keys: "factors", "loadings", and "factor_sums"
+  factor_sums is the "n" in the multinomial
+  (ie the sum of the counts per observations)
+
+  2. Soft clustering of features (lda_mode=False):
+  Rows of loadings matrix sum to one, cols of factors matrix sum to one
+  Returns a dict with keys: "factors", "loadings", and "feature_sums"
+  feature_sums is similar to an intercept term for each feature
+  """
+  if lda_mode:
+    W,eF,eFsum,Wsum = rescale_as_lda(factors,loadings,sort=sort)##!!!!
+    return {"factors":eF,"loadings":W,"totals1":eFsum,"totals2":Wsum}
+  else: #spatialDE mode
+    eF,W,Wsum,eFsum = rescale_as_lda(loadings,factors,sort=sort)
+    return {"factors":eF,"loadings":W,"totals1":Wsum,"totals2":eFsum}
+
+
+def rescale_as_lda(factors,loadings,sort=True):
+  """
+  Rescale nonnegative factors and loadings matrices to be
+  comparable to LDA:
+  Rows of factor matrix sum to one, cols of loadings matrix sum to one
+  """
+  W = postprocess.deepcopy(loadings)
+  eF = postprocess.deepcopy(factors)
+  W,wsum = postprocess.normalize_cols(W)
+  #eF1,eFsum = postprocess.normalize_rows(eF*wsum)##??
+  eF,eFsum = postprocess.normalize_rows(eF*wsum)##??
+  if sort:
+    o = np.argsort(-eF.sum(axis=0))
+    return W[:,o],eF[:,o],eFsum
+  else:
+    return W,eF,eFsum,wsum
+
+
+
+
