70dcd340b3999238cb4a865e63385ab83d78ed00
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2023-05-09 09:54:02 -0400

    Add files via upload

--- mouse_sagittal_data_analysis/step2_initialization.py
+++ mouse_sagittal_data_analysis/step2_initialization.py
@@ -1,7 +1,6 @@
 
 
 
-import sys
 
 
 dir_mNSF_functions='/dcs04/hansen/data/ywang/ST/mNSF_package/functions/'
@@ -21,8 +20,8 @@ dpth='/dcs04/hansen/data/ywang/ST/data_10X_ST/mouse_Sagittal/put/'
 
 ## sample 1
 #ad = read_h5ad((dpth,"data_s1.h5ad"))
-Y=pd.read_csv(path.join(dpth,'Y_sample1.csv')
-X=pd.read_csv(path.join(dpth,'X_sample1.csv')
+Y=pd.read_csv(path.join(dpth,'Y_sample1.csv'))
+X=pd.read_csv(path.join(dpth,'X_sample1.csv'))
 X = preprocess.rescale_spatial_coords(X)
 X=X.to_numpy()
 
@@ -45,8 +44,8 @@ D1=D
 ## sample 2
 #ad = read_h5ad((dpth,"data_s2.h5ad"))
 ## sample 2
-Y=pd.read_csv(path.join(dpth,'Y_sample2.csv')
-X=pd.read_csv(path.join(dpth,'X_sample2.csv')
+Y=pd.read_csv(path.join(dpth,'Y_sample2.csv'))
+X=pd.read_csv(path.join(dpth,'X_sample2.csv'))
 X = preprocess.rescale_spatial_coords(X)
 X=X.to_numpy()
 
@@ -122,13 +121,15 @@ D2["Z"]=D2['X']
 ###################
 ###################
 
+L=8
+J=500
 
-list_D__=[D1,D2,D3]
+list_D__=[D1,D2]
 
 
 
-fit1=pf_ori_mod.ProcessFactorization(J,L,D1['Z'],psd_kernel=ker,nonneg=True,lik="poi")
-fit2=pf_ori_mod.ProcessFactorization(J,L,D2['Z'],psd_kernel=ker,nonneg=True,lik="poi")
+fit1=pf.ProcessFactorization(J,L,D1['Z'],psd_kernel=ker,nonneg=True,lik="poi")
+fit2=pf.ProcessFactorization(J,L,D2['Z'],psd_kernel=ker,nonneg=True,lik="poi")
 
 
 #fit1.init_loadings(D["Y"],X=X,sz=D["sz"],shrinkage=0.3)
@@ -140,7 +141,7 @@ fit2.init_loadings(D2["Y"],X=D2['X'],sz=D2["sz"],shrinkage=0.3)
 
 
 ##could remove omega in this step
-fit12_=pf_fit12_fullSample_2_3.ProcessFactorization_fit12(J,L,
+fit12_=pf_multiSample.ProcessFactorization_fit12(J,L,
   np.concatenate((D1['Z'], D2['Z']), axis=0),
   nsample=nsample_,
   psd_kernel=ker,nonneg=True,lik="poi")
@@ -191,12 +192,12 @@ fit2.W.assign(W) #LxM
 
 
 kkk=0
-list_para_tmp=training.store_paras_from_tf_to_np(fit1)
+list_para_tmp=training_multiSample.store_paras_from_tf_to_np(fit1)
 save_object(list_para_tmp, 'list_para_'+ str(kkk+1) +'.pkl')
 list_para_tmp["Kuu_chol"].shape
 
 kkk=1
-list_para_tmp=training.store_paras_from_tf_to_np(fit2)
+list_para_tmp=training_multiSample.store_paras_from_tf_to_np(fit2)
 save_object(list_para_tmp, 'list_para_'+ str(kkk+1) +'.pkl')
 
 
--- mouse_sagittal_data_analysis/step3_fitModel.py
+++ mouse_sagittal_data_analysis/step3_fitModel.py
@@ -1,15 +1,14 @@
 
 ##################################################################
-import sys
+
 
 dir_mNSF_functions='/dcs04/hansen/data/ywang/ST/mNSF_package/functions/'
 sys.path.append(dir_mNSF_functions)
+
+
+
+
 import load_packages # load functions from mNSF and NSF, as well as the dependencies
-
-
-
-
-
 
 
 
@@ -20,8 +19,8 @@ dpth='/dcs04/hansen/data/ywang/ST/data_10X_ST/mouse_Sagittal/put/'
 
 ## sample 1
 #ad = read_h5ad((dpth,"data_s1.h5ad"))
-Y=pd.read_csv(path.join(dpth,'/Y_sample1.csv')
-X=pd.read_csv(path.join(dpth,'X_sample1.csv')
+Y=pd.read_csv(path.join(dpth,'Y_sample1.csv'))
+X=pd.read_csv(path.join(dpth,'X_sample1.csv'))
 X = preprocess.rescale_spatial_coords(X)
 X=X.to_numpy()
 
@@ -44,8 +43,8 @@ D1=D
 ## sample 2
 #ad = read_h5ad((dpth,"data_s2.h5ad"))
 ## sample 2
-Y=pd.read_csv(path.join(dpth,'Y_sample2.csv')
-X=pd.read_csv(path.join(dpth,'X_sample2.csv')
+Y=pd.read_csv(path.join(dpth,'Y_sample2.csv'))
+X=pd.read_csv(path.join(dpth,'X_sample2.csv'))
 X = preprocess.rescale_spatial_coords(X)
 X=X.to_numpy()
 
@@ -92,7 +91,7 @@ D1["Z"]=D1['X']
 D2["Z"]=D2['X']
 
 
-
+L=8
 
 ###################
 ###################
@@ -100,20 +99,20 @@ D2["Z"]=D2['X']
 
 
 list_D__=[D1,D2]
-psutil.Process().memory_info().rss / (1024 * 1024 * 1024)
+
 
 D12=list_D__[1]
 
 
-fit12=pf_ori_mod.ProcessFactorization(J,L,D12['Z'],psd_kernel=ker,nonneg=True,lik="poi")
+fit12=pf.ProcessFactorization(J,L,D12['Z'],psd_kernel=ker,nonneg=True,lik="poi")
 fit12.init_loadings(D12["Y"],X=D12['X'],sz=D12["sz"],shrinkage=0.3)
 
 ## 1500 spots per sample show memory issues after running 15mins (a few iterations throughout the 12 samples)
-mpth = path.join(pth,"_models_L8_")#100 batches - finished running one round of sample 1-12, stuck at sample 2 at the 2nd round
+mpth = path.join("models_L8_")#100 batches - finished running one round of sample 1-12, stuck at sample 2 at the 2nd round
 #mpth = path.join(pth,"models__nsample_memorySaving_qsub_gpu_fullDasta_L7_byBatches_v9_rep2_")#10 batches
 
 pp = fit12.generate_pickle_path("constant",base=mpth)
-misc.mkdir_p(plt_pth) 
+
 misc.mkdir_p(mpth)
 misc.mkdir_p(pp)
 
@@ -124,19 +123,27 @@ list_Ds_train_batches_=[D1_train,D2_train]
 #del ad
 #del D1,D2,D3
 
-tro_ = training_fullData_12.ModelTrainer(fit12,pickle_path=pp)#run without error message
+tro_ = training_multiSample.ModelTrainer(fit12,pickle_path=pp)#run without error message
 #tro = training.ModelTrainer.from_pickle(pp)
 
 
 
 
-tro_1 = training_ori.ModelTrainer(fit12,pickle_path=pp)#run without error message
-del fit12
+tro_1 = training.ModelTrainer(fit12,pickle_path=pp)#run without error message
+#del fit12
 
+import time
 
+time.time()
+1683640214.0678272
 
 for kkk in range(0,50):
 	fit =tro_.train_model([tro_1],
         	list_Ds_train_batches_,list_D__)## 1 iteration
+        	
 
+time.time()
+1683640273.4912817
+
+#59.4 seconds
 
