d5f67ce925f3c91bd13d7d739620c174c8db5210
kwoyshner <kwoyshn1@jh.edu>
2023-06-23 13:19:51 -0400

    add axes for memory plots

--- mouse_sagittal_data_analysis_benchmarkMem/plot_res.py
+++ mouse_sagittal_data_analysis_benchmarkMem/plot_res.py
@@ -8,7 +8,9 @@ import matplotlib.pyplot as plt
 #%%
 dataFile = "results/memoryUsage_mouseSagittal_n3_iterL_runSeperately_SOB_062323.csv"
 X = "L"
+X_label = "L: num patterns"
 Y = "peak_after_training"
+Y_label = "Peak memory usage after training (GB)"
 savefig = True
 savename = "mouseSagittal_n3_iterL_SOB"
 
@@ -16,6 +18,8 @@ savename = "mouseSagittal_n3_iterL_SOB"
 data = pd.read_csv(dataFile)
 plt.plot(data[X], data[Y])
 plt.title(savename)
+plt.xlabel(X_label)
+plt.ylabel(Y_label)
 
 if savefig:
     plt.savefig("plots/" + savename+".png")
