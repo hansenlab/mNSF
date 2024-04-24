cc8d4f7151f6887832ec7e9e8f6a0cf5e683fcc5
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2024-04-24 11:17:59 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,9 @@ setup(
   url = 'https://github.com/hansenlab/mNSF/',   
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['spatial', 'factorization', 'multi-sample'],   
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
+install_requires=[
+          'python=3.10','anndata', 'click', 'matplotlib', 'numpy', 'pandas', 'pip', 
+          'scanpy', 'squidpy', 'tensorflow=2.13', 'tensorflow-probability=0.21'],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
 
