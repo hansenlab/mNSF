eff80fed12160dde8b99ca1819f7faff3d759f3b
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2024-04-30 06:14:55 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
   packages = ['mNSF'],   
   version = '0.1.0',     
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'multi-sample non-negative spatial factorization',   # Give a short description about your library
+  description = 'multi-sample non-negative spatial factorization',   #
   author = "Yi Wang, Kyla Woyshner, Chaichontat Sriworarat, Loyal Goff, Genevieve Stein-O'Brien, Kasper D. Hansen",                   
   author_email = 'yiwangthu4@gmail.com',      
   url = 'https://github.com/hansenlab/mNSF/',   
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    
   keywords = ['spatial', 'factorization', 'multi-sample'],   
   install_requires=[
           'python==3.10','anndata', 'click', 'matplotlib', 'numpy', 'pandas', 'pip', 
@@ -16,11 +16,10 @@ setup(
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
 
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Intended Audience :: Developers',     
     'Topic :: Software Development :: Build Tools',
 
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-
+    'License :: OSI Approved :: MIT License',   
     'Programming Language :: Python :: 3.10',      
 
   ],
