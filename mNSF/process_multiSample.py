5c13a80148754fcb2e87e23ed3a38449a04dcbca
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2024-11-12 15:58:21 +0100

    Update process_multiSample.py

--- mNSF/process_multiSample.py
+++ mNSF/process_multiSample.py
@@ -131,8 +131,6 @@ def get_listX_chunked(list_X_,list_nchunk=None):
 	for ksample in range(0,nsample):
 		X=list_X_[ksample]
 		nchunk = list_nchunk[ksample]
-		X = D['X']
-		Y = D['Y']
 		nspot = X.shape[1]
 		nspot_perChunk = int(nspot/nchunk)
 		for kchunk in range(0,nchunk):
